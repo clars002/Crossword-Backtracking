1e200e0152b6028e58746e601aa02f858b4cbc77
Caelan Larsen <clars002@odu.edu>
2024-10-25 16:15:38 -0400

    finish first pass at word list processing

--- domain_split.py
+++ domain_split.py
@@ -1,5 +1,7 @@
+import os
+
 def split_words(path_to_list: str, write_to_files: bool = False):
-    word_lists = [[] for i in range(45)]
+    word_lists = [[] for i in range(46)] # apparently the longest word in the English dictionary is 45 letters long
                 
     with open(path_to_list, "r") as file:
         for line in file:
@@ -7,8 +9,19 @@ def split_words(path_to_list: str, write_to_files: bool = False):
             length = len(stripped)
             word_lists[length].append(stripped)
 
-    # if write_to_files:
-    #     for list in word_lists:
+    
+    base_name = os.path.splitext(os.path.basename(path_to_list))[0]
 
+    if write_to_files:
+        for i in range(46):
+            current_list = word_lists[i]
+            if len(current_list) == 0:
+                continue
+
+            new_file_path = "docs/generated/" + base_name + f"_len_{i}" + ".txt"
+            if not os.path.isfile(new_file_path):
+                with open(new_file_path, "w") as new_file:
+                    for word in current_list:
+                        new_file.write(f"{word}\n")
 
     return word_lists
--- domain_split_test.py
+++ domain_split_test.py
@@ -1,13 +1,13 @@
 import domain_split as ds
 
 def main():
-    my_words = ds.split_words("docs/Words.txt")
+    my_words = ds.split_words("docs/Words.txt", True)
 
-    for i in range(4):
-        print(f"Words of length {i}:")
-        print("---------------------------")
-        for word in my_words[i]:
-            print(word)
+    # for i in range(4):
+    #     print(f"Words of length {i}:")
+    #     print("---------------------------")
+    #     for word in my_words[i]:
+    #         print(word)
 
 
 if __name__ == "__main__":
