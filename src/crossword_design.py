de7af6c5c5c36ab1cd4b4153e51fecce767f382d
Caelan Larsen <clars002@odu.edu>
2024-11-02 15:30:22 -0400

    refactoring and more args

--- src/crossword_design.py
+++ src/crossword_design.py
@@ -4,59 +4,94 @@ from typing import List
 import domain_split as ds
 import file_processor as fp
 import recursive_backtracker as rb
+from recursive_backtracker import RecursiveBacktracker
 from visualizer import Visualizer
 
 
+# ChatGPT Note: I learned how to use argparse with its help but wrote the code myself.
 def process_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("puzzle_path", type=str, help="Path to the puzzle input file.")
+    parser.add_argument(
+        "--puzzle",
+        type=str,
+        default="resources/puzzles/heart.txt",
+        help="Path to the puzzle input file.",
+    )
+    parser.add_argument(
+        "--words",
+        type=str,
+        default="resources/words/words.txt",
+        help="Path to the list of words.",
+    )
+    parser.add_argument(
+        "--heuristic",
+        type=str,
+        default="mrv",
+        help="Specify variable selection heuristic (mrv, degree, mrv+degree, first_unassigned)",
+    )
+    parser.add_argument(
+        "--disable_forward_check",
+        action="store_false",
+        help="Disable forward checking (enabled by default).",
+    )
+    parser.add_argument(
+        "--sort_domains",
+        action="store_true",
+        help="Enable sorting variable domains based on letter frequency analysis.",
+    )
+    parser.add_argument(
+        "--show_stats",
+        action="store_true",
+        help="Enable display of additional stats after a solution is found.",
+    )
     return parser.parse_args()
 
 
 def main():
     args = process_args()
 
-    word_list = ds.split_words("resources/words/words.txt")
-
-    variables = fp.read_variables(args.puzzle_path)
+    word_list = ds.split_words(args.words)
+    variables = fp.read_variables(args.puzzle)
     fp.generate_constraints(variables)
+
+    backtracker = RecursiveBacktracker(args.heuristic, args.disable_forward_check)
 
     for variable in (v for v in variables if v.letters == None):
         domain_index = variable.length
-        variable_domain = word_list[domain_index][:]
+        variable.domain = word_list[domain_index][:]
 
-        critical_indices = []
+        if args.sort_domains:
+            critical_indices = []
 
-        for constraint in variable.constraints:
-            critical_index = constraint.index_self
-            critical_indices.append(critical_index)
+            for constraint in variable.constraints:
+                critical_index = constraint.index_self
+                critical_indices.append(critical_index)
 
-        def evaluate_string(
-            s: str, critical_indices: List[int], char_values: List[float]
-        ):
-            value = 0
+            def evaluate_string(
+                s: str, critical_indices: List[int], char_values: List[float]
+            ):
+                value = 0
 
-            for i in critical_indices:
-                critical_char = s[i]
-                alpha_position = ord(critical_char) - ord("a")  # ChatGPT line
-                value += char_values[alpha_position]
+                for i in critical_indices:
+                    critical_char = s[i]
+                    alpha_position = ord(critical_char) - ord("a")  # ChatGPT line
+                    value += char_values[alpha_position]
 
-            return value
+                return value
 
-        sorted_variable_domain = sorted(
-            variable_domain,
-            key=lambda s: evaluate_string(s, critical_indices, rb.letter_frequencies),
-            reverse=True,
-        )
-
-        variable.domain = sorted_variable_domain
+            variable.domain.sort(
+                key=lambda s: evaluate_string(
+                    s, critical_indices, rb.letter_frequencies
+                ),
+                reverse=True,
+            )
 
     print("Finished initializing variables, constraints, and domains.")
 
     used_values = []
 
     print("Commencing recursive backtracking. Stand by...")
-    solution = rb.recursive_backtracking(variables, used_values)
+    solution = backtracker.recursive_backtracking(variables, used_values)
 
     if solution != "Failure" and solution != None:
         print("Solution found!")
--- src/recursive_backtracker.py
+++ src/recursive_backtracker.py
@@ -1,5 +1,6 @@
 import copy
 import random as rand
+from typing import Callable, List
 
 from word import Constraint, Word
 
@@ -33,135 +34,167 @@ letter_frequencies = [
 ]
 
 
-def is_complete(assignment: List[Word]):
-    for word in assignment:
-        if word.letters == None:
-            return False
-    return True
+class RecursiveBacktracker:
+    def __init__(self, heuristic: str = "mrv", forward_checking: bool = True):
+        self.heuristic = heuristic
+        self.forward_checking = forward_checking
+        self.select_unassigned_variable = RecursiveBacktracker._get_selection_function(
+            heuristic
+        )
 
+    def recursive_backtracking(self, assignment: List[Word], used_values: List[str]):
+        if RecursiveBacktracker._is_complete(assignment):
+            return assignment
 
-def satisfies_constraints(value: str, variable: Word):
-    for constraint in variable.constraints:
-        other_value = constraint.other_word.letters
-        if (
-            other_value != None
-            and value[constraint.index_self] != other_value[constraint.index_other]
-        ):
-            return False
-    return True
+        var = self.select_unassigned_variable(assignment)
 
+        for value in (v for v in var.domain if v not in used_values):  # ChatGPT line
+            dead_end = False
 
-def first_unassigned(assignment: List[Word]):
-    for word in assignment:
-        if word.letters == None:
-            return word
-    return None
-
-
-def minimum_remaining_values(assignment: List[Word]):
-    minimum_remaining = -1
-    selection = None
-
-    for word in (w for w in assignment if w.letters == None):
-        remaining = len(word.domain)
-        if minimum_remaining == -1 or remaining < minimum_remaining:
-            minimum_remaining = remaining
-            selection = word
-
-    return selection
-
-
-def degree_heuristic(assignment: List[Word]):
-    most_constraints = -1
-    selection = None
-
-    for word in (w for w in assignment if w.letters == None):
-        constraint_count = len(word.constraints)
-        if constraint_count > most_constraints:
-            most_constraints = constraint_count
-            selection = word
-
-    return selection
-
-
-def minimum_remaining_and_degree(assignment: List[Word]):
-    minimum_remaining = -1
-    selection = []
-
-    for word in (w for w in assignment if w.letters == None):
-        remaining = len(word.domain)
-        if minimum_remaining == -1 or remaining < minimum_remaining:
-            minimum_remaining = remaining
-
-    for word in (w for w in assignment if w.letters == None):
-        remaining = len(word.domain)
-        if remaining == minimum_remaining:
-            selection.append(word)
-
-    most_constraints = -1
-    final_choice = None
-
-    for choice in selection:
-        constraint_count = len(choice.constraints)
-        if constraint_count > most_constraints:
-            most_constraints = constraint_count
-            final_choice = choice
-
-    return final_choice
-
-
-def update_other_domain(value: str, constraint: Constraint):
-    other_word = constraint.other_word
-    old_domain = other_word.domain
-    critical_index = constraint.index_other
-    critical_char = value[constraint.index_self]
-
-    new_domain = [
-        value for value in old_domain if value[critical_index] == critical_char
-    ]
-
-    return new_domain
-
-
-def recursive_backtracking(assignment: List[Word], used_values: List[str]):
-    if is_complete(assignment):
-        return assignment
-
-    var = minimum_remaining_values(assignment)
-
-    for value in (v for v in var.domain if v not in used_values):  # ChatGPT line
-        dead_end = False
-
-        if satisfies_constraints(value, var):
-            old_domains = []
-            new_domains = []
-            for constraint in var.constraints:
-                other_word = constraint.other_word
-                if constraint.other_word.letters == None:
-                    updated_domain = update_other_domain(value, constraint)
-                    old_domains.append((other_word, other_word.domain))
-                    if len(updated_domain) != 0:
+            if RecursiveBacktracker._satisfies_constraints(value, var):
+                old_domains = []
+                new_domains = []
+                for constraint in var.constraints:
+                    other_word = constraint.other_word
+                    if constraint.other_word.letters == None:
+                        updated_domain = RecursiveBacktracker._update_other_domain(
+                            value, constraint
+                        )
+                        old_domains.append((other_word, other_word.domain))
                         new_domains.append((other_word, updated_domain))
-                    else:
-                        dead_end = True
-                        # print("Forward looking has detected a dead end; avoiding it.")
-                        break  # Breaks out of the for constraint loop (only) (I think)
+                        if self.forward_checking and len(updated_domain) == 0:
+                            dead_end = True
+                            # print("Forward looking has detected a dead end; avoiding it.")
+                            break  # Breaks out of the for constraint loop (only) (I think)
 
-            if not dead_end:
-                var.letters = value
-                used_values.append(value)
+                if not dead_end:
+                    var.letters = value
+                    used_values.append(value)
 
-                for word, new_domain in new_domains:
-                    word.domain = new_domain
+                    for word, new_domain in new_domains:
+                        word.domain = new_domain
 
-                result = recursive_backtracking(assignment, used_values)
-                if result != None:
-                    return result
+                    result = self.recursive_backtracking(assignment, used_values)
+                    if result != None:
+                        return result
 
-                var.letters = None
-                used_values.remove(value)
+                    var.letters = None
+                    used_values.remove(value)
 
-                for word, old_domain in old_domains:
-                    word.domain = old_domain
+                    for word, old_domain in old_domains:
+                        word.domain = old_domain
 
-    return None
+        return None
+
+    @staticmethod
+    def _get_selection_function(heuristic: str) -> Callable[[List["Word"]], "Word"]:
+        if heuristic in (
+            "mrv",
+            "MRV",
+            "minimum_remaining_values",
+            "minimum_remaining",
+            "min_rem",
+        ):
+            return RecursiveBacktracker._minimum_remaining_values
+        elif heuristic in ("degree", "deg", "d"):
+            return RecursiveBacktracker._degree_heuristic
+        elif heuristic in (
+            "mrv+degree",
+            "mrv_degree",
+            "mrv_deg",
+            "mrv-degree",
+            "mrv-deg",
+        ):
+            return RecursiveBacktracker._minimum_remaining_and_degree
+        else:
+            return RecursiveBacktracker._first_unassigned
+
+    @staticmethod
+    def _is_complete(assignment: List[Word]):
+        for word in assignment:
+            if word.letters == None:
+                return False
+        return True
+
+    @staticmethod
+    def _satisfies_constraints(value: str, variable: Word):
+        for constraint in variable.constraints:
+            other_value = constraint.other_word.letters
+            if (
+                other_value != None
+                and value[constraint.index_self] != other_value[constraint.index_other]
+            ):
+                return False
+        return True
+
+    @staticmethod
+    def _first_unassigned(assignment: List[Word]):
+        for word in assignment:
+            if word.letters == None:
+                return word
+        return None
+
+    @staticmethod
+    def _minimum_remaining_values(assignment: List[Word]):
+        minimum_remaining = -1
+        selection = None
+
+        for word in (w for w in assignment if w.letters == None):
+            remaining = len(word.domain)
+            if minimum_remaining == -1 or remaining < minimum_remaining:
+                minimum_remaining = remaining
+                selection = word
+
+        return selection
+
+    @staticmethod
+    def _degree_heuristic(assignment: List[Word]):
+        most_constraints = -1
+        selection = None
+
+        for word in (w for w in assignment if w.letters == None):
+            constraint_count = len(word.constraints)
+            if constraint_count > most_constraints:
+                most_constraints = constraint_count
+                selection = word
+
+        return selection
+
+    @staticmethod
+    def _minimum_remaining_and_degree(assignment: List[Word]):
+        minimum_remaining = -1
+        selection = []
+
+        for word in (w for w in assignment if w.letters == None):
+            remaining = len(word.domain)
+            if minimum_remaining == -1 or remaining < minimum_remaining:
+                minimum_remaining = remaining
+
+        for word in (w for w in assignment if w.letters == None):
+            remaining = len(word.domain)
+            if remaining == minimum_remaining:
+                selection.append(word)
+
+        most_constraints = -1
+        final_choice = None
+
+        for choice in selection:
+            constraint_count = len(choice.constraints)
+            if constraint_count > most_constraints:
+                most_constraints = constraint_count
+                final_choice = choice
+
+        return final_choice
+
+    @staticmethod
+    def _update_other_domain(value: str, constraint: Constraint):
+        other_word = constraint.other_word
+        old_domain = other_word.domain
+        critical_index = constraint.index_other
+        critical_char = value[constraint.index_self]
+
+        new_domain = [
+            value for value in old_domain if value[critical_index] == critical_char
+        ]
+
+        return new_domain
